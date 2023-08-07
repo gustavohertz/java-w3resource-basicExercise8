492cf232baa581c6363d20240676148de57d2b8a
Gustavo Borges Hertz <127430409+gustavohertz@users.noreply.github.com>
2023-08-07 07:54:39 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+ 
+    public static void main(String[] args) {
+
+        primeira(args);
+        segunda(args);
+        terceira(args);
+        quarta(args);
+        
+    }
+    public static void primeira(String[] args) {
+        System.out.println("   J    a   v     v  a ");
+    }
+    public static void segunda(String[] args) {
+    System.out.println("   J   a a   v   v  a a");
+    }
+    public static void terceira(String[] args) {
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+    }
+    public static void quarta(String[] args) {
+        System.out.println(" JJ  a     a   V  a     a");
+    }
+}
